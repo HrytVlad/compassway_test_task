46750f6b69fc0880be54d80d8845861846a477a7
HrytVlad <sunrises2929@gmail.com>
2023-05-29 17:54:05 +0300

    Created WeatherViewSet

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from weather_app.models import Weather
+from weather_app.serializers import WeatherSerializer
+
+
+class WeatherViewSet(viewsets.ModelViewSet):
+    queryset = Weather.objects.all()
+    serializer_class = WeatherSerializer
