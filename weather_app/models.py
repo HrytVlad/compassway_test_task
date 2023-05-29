cab87a4eef6a6f3c2ff53e2d3f66454852a030ce
HrytVlad <sunrises2929@gmail.com>
2023-05-29 17:51:24 +0300

    Created model

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Weather(models.Model):
+    date = models.DateField()
+    temperature = models.IntegerField()
+    weather_description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.date} - {self.temperature} - {self.weather_description}"
