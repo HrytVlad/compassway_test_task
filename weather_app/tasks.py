7ebe84c8bef8ccf1c9bd2bf60377fd2d01edde22
HrytVlad <sunrises2929@gmail.com>
2023-05-31 10:03:33 +0300

    Added celery and tasks

---
+++ weather_app/tasks.py
@@ -0,0 +1,21 @@
+from celery.schedules import crontab
+
+from .parser import parse_weather
+
+from celery import shared_task, Celery
+
+app = Celery()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(crontab(minute=1))
+
+
+#
+# @shared_task
+# def run_parse_weather():
+#     parse_weather()
+@app.task
+def test(*args, **kwargs):
+    print("Hello")
